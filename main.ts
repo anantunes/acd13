622df7285d5403303a2535730d872662a7fa634d
Ana <anantunes.escola@gmail.com>
2021-05-22 10:36:00 +0100

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Happy)
+    basic.clearScreen()
+    basic.pause(100)
+    basic.showIcon(IconNames.Sad)
+    basic.pause(100)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Angry)
+})
